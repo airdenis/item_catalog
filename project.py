a1fd60e1fc917c6adb6ceed14225a80005bfa87a
vagrant <vagrant@vagrant.vm>
2018-10-31 03:40:25 +0000

    feat: add delete function.

--- project.py
+++ project.py
@@ -26,8 +26,64 @@ def categoriesDashboard():
 @app.route('/catalog/<string:category_name>/items/')
 def categoryItems(category_name):
     category = session.query(Category).filter_by(name=category_name).one()
-    items = session.query(Item).filter_by(category_name=category.name)
+    items = session.query(Item).filter_by(
+            category_name=category.name
+            ).order_by('time desc')
     return render_template('items.html', category=category, items=items)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/')
+def itemDescription(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(Item).filter_by(
+            name=item_name,
+            category_name=category.name
+            ).one()
+    return render_template(
+            'itemdescription.html',
+            category=category,
+            item=item
+            )
+
+
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+def newItem():
+    if request.method == 'POST':
+        new_item = Item(
+                name=request.form['name'],
+                description=request.form['description'],
+                category_name=request.form['category']
+                )
+        session.add(new_item)
+        session.commit()
+        session.close()
+        return redirect(url_for('categoriesDashboard'))
+    else:
+        return render_template('newitem.html')
+
+
+@app.route(
+        '/catalog/<string:category_name>/<string:item_name>/delete/',
+        methods=['GET', 'POST']
+        )
+def deleteItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    delete_item = session.query(Item).filter_by(
+            name=item_name,
+            category_name=category.name
+            ).one()
+    print category.name
+    if request.method == 'POST':
+        session.delete(delete_item)
+        session.commit()
+        session.close()
+        return redirect(url_for('categoryItems', category_name=category_name))
+    else:
+        return render_template(
+                'deleteitem.html',
+                category=category,
+                item=delete_item
+                )
 
 
 if __name__ == '__main__':
