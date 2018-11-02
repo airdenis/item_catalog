316a7cba63bf6bfcbf18eec6501f696559f6df07
vagrant <vagrant@vagrant.vm>
2018-11-02 00:05:10 +0000

    feat: add edititem functionality.

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 from flask import (
         Flask, render_template, redirect, request, url_for, flash, jsonify
         )
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from database_setup import Base, Category, Item
@@ -52,7 +52,8 @@ def newItem():
         new_item = Item(
                 name=request.form['name'],
                 description=request.form['description'],
-                category_name=request.form['category']
+                category_name=request.form['category'],
+                time=func.now()
                 )
         session.add(new_item)
         session.commit()
@@ -62,28 +63,38 @@ def newItem():
         return render_template('newitem.html')
 
 
+@app.route('/catalog/<string:item_name>/edit', methods=['GET', 'POST'])
+def editItem(item_name):
+    edit_item = session.query(Item).filter_by(name=item_name).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            edit_item.name = request.form['name']
+        if request.form['description']:
+            edit_item.description = request.form['description']
+        if request.form['category']:
+            edit_item.category_name = request.form['category']
+        edit_item.time = func.now()
+        session.add(edit_item)
+        session.commit()
+        session.close()
+        return redirect(url_for('categoriesDashboard'))
+    else:
+        return render_template('edititem.html', item=edit_item)
+
+
 @app.route(
-        '/catalog/<string:category_name>/<string:item_name>/delete/',
+        '/catalog/<string:item_name>/delete/',
         methods=['GET', 'POST']
         )
-def deleteItem(category_name, item_name):
-    category = session.query(Category).filter_by(name=category_name).one()
-    delete_item = session.query(Item).filter_by(
-            name=item_name,
-            category_name=category.name
-            ).one()
-    print category.name
+def deleteItem(item_name):
+    delete_item = session.query(Item).filter_by(name=item_name).one()
     if request.method == 'POST':
         session.delete(delete_item)
         session.commit()
         session.close()
-        return redirect(url_for('categoryItems', category_name=category_name))
+        return redirect(url_for('categoriesDashboard'))
     else:
-        return render_template(
-                'deleteitem.html',
-                category=category,
-                item=delete_item
-                )
+        return render_template('deleteitem.html', item=delete_item)
 
 
 if __name__ == '__main__':
